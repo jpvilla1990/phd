52c1ec20ea11571670c5f0e7218d05f7afe45e64
Villa Serna Juan Pablo <JuanPablo.VillaSerna@elektrobit.com>
2025-01-13 23:34:41 +0100

    adding iterator for etth repo

--- src/datasets/datasetIterator.py
+++ src/datasets/datasetIterator.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from exceptions.datasetException import DatasetException
 
 class DatasetIterator(object):
@@ -11,13 +12,67 @@ class DatasetIterator(object):
     def __str__(self) -> str:
         return self.__name
 
-    def loadSamples(self, subdataset : str, numberSamples : int) -> any:
+    def __getExtension(self, subdataset : str) -> str:
         """
-        Method to load samples
+        Method to get extension of subdataset
         """
+        return self.__datasets[subdataset].split(".")[-1].lower()
+
+    def getAvailableFeatures(self, subdataset : str) -> list:
+        """
+        Method to get available features
+        """
+        extension : str = self.__getExtension(subdataset)
+
+        features : list = None
+
         if subdataset not in self.__datasets:
             raise DatasetException(
                 f"Subdataset {subdataset} does not exists in dataset {self}"
             )
 
-        return self.__datasets[subdataset]
+        if extension == "csv":
+            features = pd.read_csv(self.__datasets[subdataset], nrows=0).columns.tolist()
+
+        else:
+            raise DatasetException(
+                f"Extension {extension} not supported, review the subdataset path {self.__datasets[subdataset]}"
+            )
+
+        return features
+
+    def loadSamples(self, subdataset : str, numberSamples : int = 0, features : list = []) -> pd.core.frame.DataFrame:
+        """
+        Method to load samples
+        """
+        sample : pd.core.frame.DataFrame = None
+        if subdataset not in self.__datasets:
+            raise DatasetException(
+                f"Subdataset {subdataset} does not exists in dataset {self}"
+            )
+
+        extension : str = self.__getExtension(subdataset)
+
+        if extension == "csv":
+            sample = pd.read_csv(self.__datasets[subdataset])
+
+        else:
+            raise DatasetException(
+                f"Extension {extension} not supported, review the subdataset path {self.__datasets[subdataset]}"
+            )
+
+        if len(features) > 0:
+            availableFeatures : list = self.getAvailableFeatures(subdataset)
+            for feature in features:
+                if feature not in availableFeatures:
+                    raise DatasetException(
+                        f"Feature {feature} does not exists in subdataset {subdataset}"
+                    )
+            sample = sample[features]
+
+        if numberSamples <= 0:
+            return sample
+        elif numberSamples >= len(sample):
+            return sample
+        else:
+            return sample.sample(n=numberSamples)
--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from datasets.datasets import Datasets
 dataset : Datasets = Datasets()
 
 iterator = dataset.loadDataset("ET")
-print(iterator.loadSamples("ETTh2s", 4))
+print(iterator.loadSamples("ETTh2", 4, ["HUFL", "HULL"]))
