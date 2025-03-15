af5d4bbda7a10ee36041ae65ecb42009cc13167a
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2025-03-15 23:45:26 +0000

    results using seed 42

--- src/main.py
+++ src/main.py
@@ -1,19 +1,5 @@
 from evaluation.evaluation import Evaluation
 
-CONTEXT : int = 32
-PREDICTION : int = 16
-NUMBER_SAMPLES : int = 100
-DATASET : str = "ET"
-COLLECTION : str = "moiraiMoEETCosine_32_16"
-
 evaluation : Evaluation = Evaluation()
 
-report : dict = evaluation.evaluateMoiraiMoERag(
-    CONTEXT,
-    PREDICTION,
-    NUMBER_SAMPLES,
-    DATASET,
-    COLLECTION,
-)
-
-print(report)
+report : dict = evaluation.compileReports() # Reports will be located in src/data/reports.pdf
