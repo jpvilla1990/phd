351269047d7ce296addacbe9a457f4134e401c7e
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2025-03-22 19:26:04 +0000

    including traffic

--- src/main.py
+++ src/main.py
@@ -2,4 +2,14 @@ from evaluation.evaluation import Evaluation
 
 evaluation : Evaluation = Evaluation()
 
-report : dict = evaluation.compileReports(reportOriginName="evaluationReportsMoiraiMoE",reportTargetName="evaluationFinalReport") # Reports will be located in src/data/reports.pdf
+CONTEXT : int = 128
+PREDICTION : int = 16
+NUMBER_SAMPLES : int = 100
+DATASET : str = "traffic"
+
+report : dict = evaluation.evaluateMoiraiMoE(
+    CONTEXT,
+    PREDICTION,
+    NUMBER_SAMPLES,
+    DATASET,
+)
