bd224c7d3871849a493d0b356743adcccfdb238f
Juan Pablo Villa Serna <JuanPablo.VillaSerna@elektrobit.com>
2025-03-22 11:15:47 +0100

    exprimenta traffic

--- src/main.py
+++ src/main.py
@@ -1,37 +1,5 @@
 from evaluation.evaluation import Evaluation
 
-CONTEXT : int = 32
-PREDICTION : int = 16
-DATASET : str = "ET"
-
 evaluation : Evaluation = Evaluation()
 
-report : dict = evaluation.evaluateChatTimes(
-    CONTEXT,
-    PREDICTION,
-    DATASET,
-)
-
-CONTEXT : int = 64
-PREDICTION : int = 16
-DATASET : str = "ET"
-
-evaluation : Evaluation = Evaluation()
-
-report : dict = evaluation.evaluateChatTimes(
-    CONTEXT,
-    PREDICTION,
-    DATASET,
-)
-
-CONTEXT : int = 128
-PREDICTION : int = 16
-DATASET : str = "ET"
-
-evaluation : Evaluation = Evaluation()
-
-report : dict = evaluation.evaluateChatTimes(
-    CONTEXT,
-    PREDICTION,
-    DATASET,
-)
+report : dict = evaluation.compileReports(reportOriginName="evaluationReportsMoiraiMoE",reportTargetName="evaluationFinalReport") # Reports will be located in src/data/reports.pdf
