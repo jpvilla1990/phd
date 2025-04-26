d708eba0f3de76a9ad96a9c98fc05208b060b28e
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2025-04-26 20:00:31 +0200

    rag cross attemtion mdoudle

---
+++ src/model/ragCrossAttention.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn.functional as F
+
+class RagCrossAttention(torch.nn.Module):
+    """
+    A class to perform cross-attention on embeddings using a multi-head attention mechanism.
+    This class is designed to augment embeddings by applying a cross-attention mechanism.
+    """
+
+    def __init__(
+        self,
+        patchSize : int = 16,
+        numHeads : int = 8,
+    ):
+        """
+        Initialize the EmbeddingAugmentation class.
+
+        :param patchSize: Size of the patches to be used in the augmentation process.
+        """
+        super().__init__()
+        self.__device : str = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.__patchSize : int = patchSize
+
+        self.__crossAttentionModule : torch.nn.MultiheadAttention = torch.nn.MultiheadAttention(
+            embed_dim=patchSize,
+            num_heads=numHeads,
+            batch_first=True
+        )
+
+    def __concat(
+        self,
+        x : torch.Tensor,
+        y : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Concatenate two tensors along the last dimension.
+        This method is used to concatenate the augmented tensor with the original tensor.
+        :param x: First tensor to be concatenated.
+        :param y: Second tensor to be concatenated.
+        :return: Concatenated tensor.
+        """
+        return torch.cat((x, y), dim=-1)
+
+    def __patching(
+        self,
+        x : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Apply patching to the input tensor.
+        This method reshapes the input tensor into patches of a specified size.
+        :param x: Input tensor to be patched.
+        :return: Patches of the input tensor.
+        """
+        seqLength = x.shape[2]
+        remainder : int  = (seqLength - self.__patchSize) % self.__patchSize
+        padLen : int = self.__patchSize - remainder
+        x = F.pad(x, (0, padLen), value=0)
+        return x.unfold(dimension=2, size=self.__patchSize, step=self.__patchSize)
+    
+    def __joinPatches(
+        self,
+        x : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Join patches back to the original tensor shape.
+        This method reshapes the patches back to the original tensor shape.
+        :param x: Patches to be joined.
+        :return: Joined tensor.
+        """
+        return x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
+
+    def __normalization(
+        self,
+        x : torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Normalize the input tensor.
+        This method applies layer normalization to the input tensor.
+        :param x: Input tensor to be normalized.
+        :return: Normalized tensor and its mean and std.
+        """
+        mean : torch.Tensor = x.mean(dim=(1,2,3), keepdim=True)
+        std : torch.Tensor = x.std(dim=(1,2,3), keepdim=True) + 1e-6
+        x = (x - mean) / std
+        return x, mean, std
+    
+    def __denormalization(
+        self,
+        x : torch.Tensor,
+        mean : torch.Tensor,
+        std : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Denormalize the input tensor.
+        This method applies inverse normalization to the input tensor.
+        :param x: Input tensor to be denormalized.
+        :param mean: Mean used for normalization.
+        :param std: Standard deviation used for normalization.
+        :return: Denormalized tensor.
+        """
+        return (x * std) + mean
+    
+    def __positionalEncoding(
+        self,
+        x : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Apply positional encoding to the input tensor.
+        This method adds positional encoding to the input tensor.
+        :param x: Input tensor to be augmented with positional encoding.
+        :return: Tensor with positional encoding applied.
+        """
+        dModel : int = self.__patchSize
+        nPositions : int = x.shape[2]
+
+        pos : torch.Tensor = torch.arange(
+            nPositions,
+            dtype=torch.float32,
+            device=self.__device,
+        ).unsqueeze(1)
+
+        i : torch.Tensor = torch.arange(
+            dModel,
+            dtype=torch.float32,
+            device=self.__device,
+        ).unsqueeze(0)
+
+        angleRates : torch.Tensor = 1 / torch.pow(10000, (2 * (i // 2)) / dModel)
+
+        # Apply the formula
+        angleRads = pos * angleRates
+
+        # Apply sin to even indices in the array; cos to odd indices
+        posEncoding : torch.Tensor = torch.zeros_like(angleRads)
+        posEncoding[:, 0::2] = torch.sin(angleRads[:, 0::2])  # even dimensions
+        posEncoding[:, 1::2] = torch.cos(angleRads[:, 1::2])  # odd dimensions
+        return x + posEncoding.unsqueeze(0).unsqueeze(0)
+
+    def __crossAttention(
+        self,
+        x : torch.Tensor,
+        context : torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Apply cross-attention to the input tensor.
+        This method applies cross-attention to the input tensor using the context tensor.
+        :param x: Input tensor to be augmented with cross-attention.
+        :param context: Context tensor to be used for cross-attention.
+        :return: Tensor after applying cross-attention.
+        """
+        batchSize = x.shape[0]
+        dModel = x.shape[-1]
+        query = x.view(batchSize, -1, dModel)
+        keyValue = context.view(batchSize, -1, dModel)
+        x = self.__crossAttentionModule(query, keyValue, keyValue)
+        return x[0].unsqueeze(1)
+
+    def forward(
+        self, xInput : torch.Tensor,
+        context : torch.Tensor,
+        scores : torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Forward pass for the embedding augmentation.
+        This method applies cross-attention to the input embeddings and returns the augmented embeddings.
+        :param x: Input embeddings to be augmented. [batches, seqLength]
+        :param context: Context embeddings to be used for augmentation. [batches, k, seqLength]
+        :param scores: similarity scores to be used for augmentation. [batches, k, 1]
+        :return: Augmented embeddings after applying cross-attention.
+        """
+        xInput.to(self.__device)
+        context.to(self.__device)
+
+        x : torch.Tensor = xInput.unsqueeze(1)
+
+        x = self.__patching(x)
+        x, xMean, xStd = self.__normalization(x)
+        x = self.__positionalEncoding(x)
+
+        context = self.__patching(context * scores)
+        context, _, _ = self.__normalization(context)
+        context = self.__positionalEncoding(context)
+        x = self.__crossAttention(x, context)
+
+        x = self.__denormalization(x, xMean, xStd)
+
+        x = self.__joinPatches(x)
+
+        xAugmented : torch.Tensor = self.__concat(x.squeeze(1), xInput)
+
+        return xAugmented
+
+if __name__ == "__main__":
+    a = RagCrossAttention()
+    x = torch.randn(5, 31)
+    context = torch.randn(5, 3, 33)
+    scores = torch.randn(5, 3, 1)
+    y = a(x, context, scores)
